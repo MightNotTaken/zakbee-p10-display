259809b32cc3adc0a607797eb0afdd65205b9481
MightNotTaken <110739834+MightNotTaken@users.noreply.github.com>
2024-03-16 14:02:44 +0530

    GPIOs initialized

--- zakbee-safety-display.ino
+++ zakbee-safety-display.ino
@@ -1,6 +1,7 @@
 #include "core/core.h"
 #include "core/database.h"
 #include "data-source.h"
+#include "core/gpio.h"
 #include "config.h"
 #include "display.h"
 void setup() {  
@@ -8,6 +9,7 @@ void setup() {
 
   Display::begin();
   DataSource::begin();
+  GPIOs::begin();
   for (auto source: DataSource::sources) {
     console.log(*source);
   }
